a7589b9faef6de5047e4395cab94a85a4390dfb4
waheeb123 <84958066+waheeb123@users.noreply.github.com>
2022-12-25 00:00:23 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,31 +15,31 @@ SELECT DISTINCT flight, origin, dest, distance AS 'furthest distance' FROM fligh
 
 3 Show the total number of flights. 
 
-  SELECT COUNT(*) FROM flights;
+  SELECT COUNT * FROM flights;
  
 ---------
 
 4 Show the total number of flights by airline (carrier). 
 
-  SELECT carrier, COUNT(*) FROM flights GROUP BY carrier; 
+  SELECT carrier, COUNT * FROM flights GROUP BY carrier; 
  
 --------------
 
 5 Show all of the airlines, ordered by number of flights in descending order.  
 
- SELECT carrier, COUNT(*) AS FlightCount FROM flights GROUP BY carrier ORDER BY FlightCount DESC;
+ SELECT carrier, COUNT * AS FlightCount FROM flights GROUP BY carrier ORDER BY FlightCount DESC;
  
 ----------
 
 6 Show only the top 5 airlines, by number of flights, ordered by number of flights in descending order.   
 
-SELECT carrier, COUNT(*) AS FlightCount FROM flights GROUP BY carrier ORDER BY FlightCount DESC LIMIT 5;
+SELECT carrier, COUNT * AS FlightCount FROM flights GROUP BY carrier ORDER BY FlightCount DESC LIMIT 5;
 
 ----------
 
 7 Show only the top 5 airlines, by number of flights of distance 1,000 miles or greater, ordered by number of flights in descending order.  
 
- SELECT carrier, COUNT(*) AS FlightCount FROM flights WHERE distance >=1000 GROUP BY carrier ORDER BY FlightCount DESC LIMIT 5;
+ SELECT carrier, COUNT * AS FlightCount FROM flights WHERE distance >=1000 GROUP BY carrier ORDER BY FlightCount DESC LIMIT 5;
  
 --------
 
@@ -48,5 +48,5 @@ SELECT carrier, COUNT(*) AS FlightCount FROM flights GROUP BY carrier ORDER BY F
 Question: 
 Create a query that show top 5 airlines, by number of flights in the month of October, ordered by flights number from heights.  
 
- SELECT carrier, COUNT(*) as FlightCount FROM flights WHERE MONTH = 10 GROUP BY carrier ORDER BY FlightCount DESC LIMIT 5; 
+ SELECT carrier, COUNT * as FlightCount FROM flights WHERE MONTH = 10 GROUP BY carrier ORDER BY FlightCount DESC LIMIT 5; 
  
